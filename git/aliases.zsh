e666b64dd91d7086e65cfa13ec8f1851a8849793
Lincoln Mullen <lincoln@lincolnmullen.com>
2013-03-16 10:18:20 -0400

    Add alias for gitignore

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -20,3 +20,7 @@ alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C
 alias gp='git push origin HEAD'
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+
+
+# Add a file to a .gitignore file
+alias gi="echo $1 >> .gitignore"
