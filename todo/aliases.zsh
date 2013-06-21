6f3af5ebce459e586ec2ab53c8aa5bfbd0d52715
Lincoln Mullen <lincoln@lincolnmullen.com>
2013-06-21 00:57:50 -0400

    Deleted Mac specific settings

--- todo/aliases.zsh
+++
@@ -1,3 +0,0 @@
-alias t='/usr/local/Cellar/todo-txt/2.9/bin/todo.sh'
-alias tpv='todo.sh projectview -+DTBM -+divhum -+hacks -@deferred -+chores | more -R'
-export TODOTXT_DEFAULT_ACTION=lsp  # list tasks tagged for today by default
