44a51f473866f6d8909a59e72d2e5eccb3fd583c
Lincoln Mullen <lincoln@lincolnmullen.com>
2013-06-22 10:21:19 -0400

    Use view instead of greedyView for multiple monitors

--- xmonad/xmonad.symlink/xmonad.hs
+++ xmonad/xmonad.symlink/xmonad.hs
@@ -1,9 +1,25 @@
 {-Setup for xmonad-}
 
 import XMonad
--- import XMonad.Hooks.DynamicLog
 import XMonad.Config.Gnome
 
-main = xmonad gnomeConfig {
-  modMask = mod4Mask
-}
+-- Imports needed for view rather than greedyView
+import qualified XMonad.StackSet as W
+import XMonad.Util.EZConfig
+
+main = xmonad $ gnomeConfig {
+      modMask = mod4Mask
+    , workspaces = myWorkspaces
+   } `additionalKeysP` myKeys
+
+myWorkspaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+myKeys = [
+    ("M-S-q", spawn "gnome-session-quit")
+  ] ++  
+
+  [ (otherModMasks ++ "M-" ++ [key], action tag)
+    | (tag, key) <- zip myWorkspaces "123456789"
+    , (otherModMasks, action) <- [ ("", windows . W.view) -- was W.greedyView
+                                    , ("S-", windows . W.shift)]
+  ]
