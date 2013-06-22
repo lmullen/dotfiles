bb5574a63aeba932ea9e33b0d172999356d1882a
Lincoln Mullen <lincoln@lincolnmullen.com>
2013-06-22 10:25:21 -0400

    Add documentation to xmonad settings

--- xmonad/xmonad.symlink/xmonad.hs
+++ xmonad/xmonad.symlink/xmonad.hs
@@ -7,17 +7,21 @@ import XMonad.Config.Gnome
 import qualified XMonad.StackSet as W
 import XMonad.Util.EZConfig
 
-main = xmonad $ gnomeConfig {
-      modMask = mod4Mask
+main = xmonad $ gnomeConfig {       -- We use gnome rather than default
+      modMask = mod4Mask            -- Use super key for mod
     , workspaces = myWorkspaces
    } `additionalKeysP` myKeys
 
 myWorkspaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 myKeys = [
-    ("M-S-q", spawn "gnome-session-quit")
-  ] ++  
-
+    -- Instead of killing window manager, log out
+    ("M-S-q", spawn "gnome-session-quit") 
+  ] ++   -- (++) is needed here because the following list comprehension
+         -- is a list, not a single key binding. Simply adding it to the
+         -- list of key bindings would result in something like [ b1, b2,
+         -- [ b3, b4, b5 ] ] resulting in a type error. (Lists must
+         -- contain items all of the same type.)
   [ (otherModMasks ++ "M-" ++ [key], action tag)
     | (tag, key) <- zip myWorkspaces "123456789"
     , (otherModMasks, action) <- [ ("", windows . W.view) -- was W.greedyView
