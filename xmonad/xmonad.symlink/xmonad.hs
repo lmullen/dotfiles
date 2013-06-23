82176252f1f5c8a733fa64e0ef444afa3b857161
Lincoln Mullen <lincoln@lincolnmullen.com>
2013-06-23 18:15:40 -0400

    Use greedy view; clean up configuration

--- xmonad/xmonad.symlink/xmonad.hs
+++ xmonad/xmonad.symlink/xmonad.hs
@@ -1,11 +1,6 @@
 import XMonad
 import XMonad.Config.Gnome
-
--- Imports needed for view rather than greedyView
-import qualified XMonad.StackSet as W
 import XMonad.Util.EZConfig
-
--- Imports need to float windows
 import XMonad.Hooks.ManageHelpers
 
 main = xmonad $ gnomeConfig {       -- We use gnome rather than default
@@ -14,26 +9,17 @@ main = xmonad $ gnomeConfig {       -- We use gnome rather than default
     , manageHook = myManageHook 
    } `additionalKeysP` myKeys
 
-myWorkspaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+myWorkspaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9:web"]
 
 myKeys = [
-    -- Instead of killing window manager, log out
-    ("M-S-q", spawn "gnome-session-quit") 
-  ] ++   -- (++) is needed here because the following list comprehension
-         -- is a list, not a single key binding. Simply adding it to the
-         -- list of key bindings would result in something like [ b1, b2,
-         -- [ b3, b4, b5 ] ] resulting in a type error. (Lists must
-         -- contain items all of the same type.)
-  [ (otherModMasks ++ "M-" ++ [key], action tag)
-    | (tag, key) <- zip myWorkspaces "123456789"
-    , (otherModMasks, action) <- [ ("", windows . W.view) -- was W.greedyView
-                                    , ("S-", windows . W.shift)]
-  ]
+  -- Instead of killing window manager, log out
+  ("M-S-q", spawn "gnome-session-quit") 
+  ] 
 
 myManageHook = composeAll [
-      manageHook gnomeConfig
-    , (className =? "Gnome-panel" <&&> title =? "Run Application") --> doCenterFloat
-    , (className =? "sublime-text-2" <&&> title =? "Open File") --> doCenterFloat
-    , (className =? "Empathy") --> doFloat
-    , (resource =? "Dialog") --> doFloat
+    manageHook gnomeConfig
+  , (className =? "Gnome-panel" <&&> title =? "Run Application") --> doCenterFloat
+  , (className =? "sublime-text-2" <&&> title =? "Open File") --> doCenterFloat
+  , (className =? "Empathy") --> doFloat
+  , (resource =? "Dialog") --> doFloat
   ]
