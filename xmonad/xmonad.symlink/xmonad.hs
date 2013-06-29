970da76d01b90ca202b1ef2f73ccb8e9aa798c5c
Lincoln Mullen <lincoln@lincolnmullen.com>
2013-06-29 11:10:17 -0400

    Float Chrome popup windows

--- xmonad/xmonad.symlink/xmonad.hs
+++ xmonad/xmonad.symlink/xmonad.hs
@@ -9,7 +9,7 @@ main = xmonad $ gnomeConfig {       -- We use gnome rather than default
     , manageHook = myManageHook 
    } `additionalKeysP` myKeys
 
-myWorkspaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9:web"]
+myWorkspaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 myKeys = [
   -- Instead of killing window manager, log out
@@ -21,5 +21,6 @@ myManageHook = composeAll [
   , (className =? "Gnome-panel" <&&> title =? "Run Application") --> doCenterFloat
   , (className =? "sublime-text-2" <&&> title =? "Open File") --> doCenterFloat
   , (className =? "Empathy") --> doFloat
+  , (stringProperty "WM_WINDOW_ROLE" =? "pop-up") --> doCenterFloat
   , (resource =? "Dialog") --> doFloat
   ]
