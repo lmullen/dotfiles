ba3ad59c1ade3f4bdc99e89c01407220943005f2
Lincoln Mullen <lincoln@lincolnmullen.com>
2013-06-29 13:49:13 -0400

    Use Chromium for a quick browser

--- xmonad/xmonad.symlink/xmonad.hs
+++ xmonad/xmonad.symlink/xmonad.hs
@@ -20,22 +20,33 @@ myKeys = [
   -- Instead of killing window manager, log out
     ("M-S-q", spawn "gnome-session-quit") 
   , ("M-S-p", scratchTerm) 
+  , ("M-S-o", scratchBrowser) 
   ] 
   where
     scratchTerm = namedScratchpadAction myScratchPads "terminal"
+    scratchBrowser = namedScratchpadAction myScratchPads "browser"
 
 -- Scratchpad configuration tips from
 -- http://pbrisbin.com/posts/scratchpad_everything
 -- http://pbrisbin.com/posts/xmonad_scratchpad
 myScratchPads = [
-  NS "terminal" spawnTerm findTerm manageTerm
+    NS "terminal" spawnTerm findTerm manageTerm
+  , NS "browser" spawnBrowser findBrowser manageBrowser
   ] where
-    spawnTerm = myTerminal ++ " --disable-factory --hide-menubar --name scratchpad"
+    spawnTerm = myTerminal ++ " --disable-factory --hide-menubar --name=scratchpad"
     findTerm  = resource    =? "scratchpad"
     manageTerm = customFloating $ W.RationalRect l t w h
       where
         h = 0.55      -- height as percent
         w = 0.55      -- width as percent
+        t = (1 - h)/2 -- centered top/bottom
+        l = (1 - w)/2 -- centered left/right
+    spawnBrowser = "chromium-browser --name=qbrowser"   -- Better if we could use Chrome
+    findBrowser  = resource =? "qbrowser"
+    manageBrowser = customFloating $ W.RationalRect l t w h
+      where
+        h = 0.98      -- height as percent
+        w = 0.98     -- width as percent
         t = (1 - h)/2 -- centered top/bottom
         l = (1 - w)/2 -- centered left/right
 
