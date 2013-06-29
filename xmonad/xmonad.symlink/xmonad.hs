4335803198f36e4d52b20d3f1808d58bcbd69fd7
Lincoln Mullen <lincoln@lincolnmullen.com>
2013-06-29 12:14:11 -0400

    Make a scratchpad, centered floating terminal

--- xmonad/xmonad.symlink/xmonad.hs
+++ xmonad/xmonad.symlink/xmonad.hs
@@ -2,6 +2,8 @@ import XMonad
 import XMonad.Config.Gnome
 import XMonad.Util.EZConfig
 import XMonad.Hooks.ManageHelpers
+import qualified XMonad.StackSet as W
+import XMonad.Util.NamedScratchpad
 
 main = xmonad $ gnomeConfig {       -- We use gnome rather than default
       modMask = mod4Mask            -- Use super key for mod
@@ -16,8 +18,26 @@ myWorkspaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 myKeys = [
   -- Instead of killing window manager, log out
-  ("M-S-q", spawn "gnome-session-quit") 
+    ("M-S-q", spawn "gnome-session-quit") 
+  , ("M-S-t", scratchTerm) 
   ] 
+  where
+    scratchTerm = namedScratchpadAction myScratchPads "terminal"
+
+-- Scratchpad configuration tips from
+-- http://pbrisbin.com/posts/scratchpad_everything
+-- http://pbrisbin.com/posts/xmonad_scratchpad
+myScratchPads = [
+  NS "terminal" spawnTerm findTerm manageTerm
+  ] where
+    spawnTerm = myTerminal ++ " --disable-factory --hide-menubar --name scratchpad"
+    findTerm  = resource    =? "scratchpad"
+    manageTerm = customFloating $ W.RationalRect l t w h
+      where
+        h = 0.5       -- height, 50% 
+        w = 0.5       -- width, 50% 
+        t = (1 - h)/2 -- centered top/bottom
+        l = (1 - w)/2 -- centered left/right
 
 myManageHook = composeAll [
     manageHook gnomeConfig
@@ -26,4 +46,4 @@ myManageHook = composeAll [
   , (className =? "Empathy") --> doFloat
   , (stringProperty "WM_WINDOW_ROLE" =? "pop-up") --> doCenterFloat
   , (resource =? "Dialog") --> doFloat
-  ]
+  ] <+> namedScratchpadManageHook myScratchPads
