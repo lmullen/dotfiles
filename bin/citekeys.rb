70cc8033c42c3c85dc2057a2c10bb8a564570f33
Lincoln Mullen <lincoln@lincolnmullen.com>
2013-07-07 20:34:57 -0400

    Quick script to grep citation keys

---
+++ bin/citekeys.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/env ruby
+
+# Grep list of citation keys
+
+# Lincoln Mullen | lincoln@lincolnmullen.com | http://lincolnmullen.com
+# MIT License <http://lmullen.mit-license.org/>
+
+require "optparse"
+
+# Default options
+options = {:keys_file => "/home/lmullen/acad/research/bib/citekeys.txt"}
+
+ARGV.options do |opts|
+  opts.banner = "Usage:  #{File.basename($PROGRAM_NAME)} [OPTIONS] search-string"
+
+  # opts.separator ""
+  # opts.separator "Specific Options:"
+
+  opts.separator "Common Options:"
+
+  opts.on( "-h", "--help",
+   "Show this message." ) do
+    puts opts
+    exit
+  end
+
+  begin
+    opts.parse!
+  rescue
+    puts opts
+    exit
+  end
+end
+
+# The string to search for
+search_string = Regexp.new ARGV[0]
+
+File.open(options[:keys_file], "r") do |file|  
+  puts file.grep(search_string)
+end
