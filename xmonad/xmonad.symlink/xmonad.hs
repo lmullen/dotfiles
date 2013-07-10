83bea1656f4fea55bd1b850a87d5253e5039e22f
Lincoln Mullen <lincoln@lincolnmullen.com>
2013-07-09 20:22:59 -0400

    Comment out scratch browser, which was a bad idea

--- xmonad/xmonad.symlink/xmonad.hs
+++ xmonad/xmonad.symlink/xmonad.hs
@@ -14,7 +14,7 @@ main = xmonad $ gnomeConfig {       -- We use gnome rather than default
 
 myTerminal = "terminator"
 
-myWorkspaces = ["1:text", "2:web", "3:research", "4:research", "5", "6", "7", "8:system", "9:misc", "NSP"]
+myWorkspaces = ["1:text", "2:web", "3:research", "4:research", "5", "6", "7:todo", "8:system", "9:misc", "NSP"]
 
 myKeys = [
   -- Instead of killing window manager, log out
@@ -24,18 +24,18 @@ myKeys = [
   , ("M-S-s", spawn "/usr/bin/sublime-text-2") 
   , ("M-S-f", spawn "gnome-search-tool")
   , ("M-S-p", scratchTerm) 
-  , ("M-S-o", scratchBrowser) 
+  --, ("M-S-o", scratchBrowser) 
   ] 
   where
     scratchTerm = namedScratchpadAction myScratchPads "terminal"
-    scratchBrowser = namedScratchpadAction myScratchPads "browser"
+    --scratchBrowser = namedScratchpadAction myScratchPads "browser"
 
 -- Scratchpad configuration tips from
 -- http://pbrisbin.com/posts/scratchpad_everything
 -- http://pbrisbin.com/posts/xmonad_scratchpad
 myScratchPads = [
     NS "terminal" spawnTerm findTerm manageTerm
-  , NS "browser" spawnBrowser findBrowser manageBrowser
+  --, NS "browser" spawnBrowser findBrowser manageBrowser
   ] where
     spawnTerm = "gnome-terminal --disable-factory --hide-menubar --name=scratchpad"
     findTerm  = resource    =? "scratchpad"
@@ -45,14 +45,14 @@ myScratchPads = [
         w = 0.55      -- width as percent
         t = (1 - h)/2 -- centered top/bottom
         l = (1 - w)/2 -- centered left/right
-    spawnBrowser = "chromium-browser --name=qbrowser"   -- Better if we could use Chrome
-    findBrowser  = resource =? "qbrowser"
-    manageBrowser = customFloating $ W.RationalRect l t w h
-      where
-        h = 0.94     -- height as percent
-        w = 0.94     -- width as percent
-        t = (1 - h)/2 -- centered top/bottom
-        l = (1 - w)/2 -- centered left/right
+    --spawnBrowser = "chromium-browser --name=qbrowser"   -- Better if we could use Chrome
+    --findBrowser  = resource =? "qbrowser"
+    --manageBrowser = customFloating $ W.RationalRect l t w h
+    --  where
+    --    h = 0.94     -- height as percent
+    --    w = 0.94     -- width as percent
+    --    t = (1 - h)/2 -- centered top/bottom
+    --    l = (1 - w)/2 -- centered left/right
 
 myManageHook = composeAll [
     manageHook gnomeConfig
