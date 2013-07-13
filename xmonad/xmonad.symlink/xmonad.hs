47149c4fa807e7fed4bfbf28242d157365a1a342
Lincoln Mullen <lincoln@lincolnmullen.com>
2013-07-12 20:32:18 -0400

    Back to gnome-terminal

--- xmonad/xmonad.symlink/xmonad.hs
+++ xmonad/xmonad.symlink/xmonad.hs
@@ -3,18 +3,21 @@ import XMonad.Config.Gnome
 import XMonad.Util.EZConfig
 import XMonad.Hooks.ManageHelpers
 import qualified XMonad.StackSet as W
-import XMonad.Util.NamedScratchpad
+--import XMonad.Util.NamedScratchpad
+import XMonad.Hooks.EwmhDesktops
 
 main = xmonad $ gnomeConfig {       -- We use gnome rather than default
-      modMask = mod4Mask            -- Use super key for mod
-    , terminal = myTerminal
-    , workspaces = myWorkspaces
-    , manageHook = myManageHook 
+    modMask           = mod4Mask            -- Use super key for mod
+    , terminal        = myTerminal
+    , workspaces      = myWorkspaces
+    , manageHook      = myManageHook 
+    , handleEventHook = fullscreenEventHook
    } `additionalKeysP` myKeys
 
-myTerminal = "terminator"
+myTerminal = "gnome-terminal"
 
-myWorkspaces = ["1:text", "2:web", "3:research", "4:research", "5", "6", "7:todo", "8:system", "9:misc", "NSP"]
+myWorkspaces = ["1:subl", "2:terminal", "3:web", "4:research", 
+  "5:research", "6", "7:todo", "8:nautilus", "9:system"]
 
 myKeys = [
   -- Instead of killing window manager, log out
@@ -23,36 +26,36 @@ myKeys = [
   , ("M-S-n", spawn "nautilus --new-window") 
   , ("M-S-s", spawn "/usr/bin/sublime-text-2") 
   , ("M-S-f", spawn "gnome-search-tool")
-  , ("M-S-p", scratchTerm) 
+  --, ("M-S-p", scratchTerm) 
   --, ("M-S-o", scratchBrowser) 
   ] 
-  where
-    scratchTerm = namedScratchpadAction myScratchPads "terminal"
+  --where
+    --scratchTerm = namedScratchpadAction myScratchPads "terminal"
     --scratchBrowser = namedScratchpadAction myScratchPads "browser"
 
 -- Scratchpad configuration tips from
 -- http://pbrisbin.com/posts/scratchpad_everything
 -- http://pbrisbin.com/posts/xmonad_scratchpad
-myScratchPads = [
-    NS "terminal" spawnTerm findTerm manageTerm
-  --, NS "browser" spawnBrowser findBrowser manageBrowser
-  ] where
-    spawnTerm = "gnome-terminal --disable-factory --hide-menubar --name=scratchpad"
-    findTerm  = resource    =? "scratchpad"
-    manageTerm = customFloating $ W.RationalRect l t w h
-      where
-        h = 0.55      -- height as percent
-        w = 0.55      -- width as percent
-        t = (1 - h)/2 -- centered top/bottom
-        l = (1 - w)/2 -- centered left/right
-    --spawnBrowser = "chromium-browser --name=qbrowser"   -- Better if we could use Chrome
-    --findBrowser  = resource =? "qbrowser"
-    --manageBrowser = customFloating $ W.RationalRect l t w h
-    --  where
-    --    h = 0.94     -- height as percent
-    --    w = 0.94     -- width as percent
-    --    t = (1 - h)/2 -- centered top/bottom
-    --    l = (1 - w)/2 -- centered left/right
+--myScratchPads = [
+--    NS "terminal" spawnTerm findTerm manageTerm
+--  , NS "browser" spawnBrowser findBrowser manageBrowser
+--  ] where
+--    spawnTerm = "gnome-terminal --disable-factory --hide-menubar --name=scratchpad"
+--    findTerm  = resource    =? "scratchpad"
+--    manageTerm = customFloating $ W.RationalRect l t w h
+--      where
+--        h = 0.55      -- height as percent
+--        w = 0.55      -- width as percent
+--        t = (1 - h)/2 -- centered top/bottom
+--        l = (1 - w)/2 -- centered left/right
+--    spawnBrowser = "chromium-browser --name=qbrowser"   -- Better if we could use Chrome
+--    findBrowser  = resource =? "qbrowser"
+--    manageBrowser = customFloating $ W.RationalRect l t w h
+--      where
+--        h = 0.94     -- height as percent
+--        w = 0.94     -- width as percent
+--        t = (1 - h)/2 -- centered top/bottom
+--        l = (1 - w)/2 -- centered left/right
 
 myManageHook = composeAll [
     manageHook gnomeConfig
@@ -62,4 +65,5 @@ myManageHook = composeAll [
   , (className =? "Empathy") --> doFloat
   , (stringProperty "WM_WINDOW_ROLE" =? "pop-up") --> doCenterFloat
   , (resource =? "Dialog") --> doFloat
-  ] <+> namedScratchpadManageHook myScratchPads
+  ] 
+  --] <+> namedScratchpadManageHook myScratchPads
