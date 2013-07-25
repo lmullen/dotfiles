91ac00fa9af2f4d4bce8ad484e6406f46a1188dc
Lincoln Mullen <lincoln@lincolnmullen.com>
2013-07-25 16:43:08 -0400

    Fixed gnome-panel for custom layouts

--- xmonad/xmonad.symlink/xmonad.hs
+++ xmonad/xmonad.symlink/xmonad.hs
@@ -1,5 +1,6 @@
 import XMonad
 import XMonad.Config.Gnome
+import XMonad.Config.Desktop
 import XMonad.Util.EZConfig
 import XMonad.Hooks.ManageHelpers
 import qualified XMonad.StackSet as W
@@ -21,7 +22,7 @@ main = xmonad $ gnomeConfig {       -- We use gnome rather than default
     , terminal        = myTerminal
     , workspaces      = myWorkspaces
     , manageHook      = myManageHook  <+> manageHook gnomeConfig
-    , layoutHook      = myLayoutHook
+    , layoutHook      = desktopLayoutModifiers $ myLayoutHook
     , handleEventHook = fullscreenEventHook <+> handleEventHook gnomeConfig
    } `additionalKeysP` myKeys
 
@@ -102,21 +103,12 @@ myManageHook = composeAll [
 -- Layouts
 
 -- Define default layouts used on most workspaces
-defaultLayouts = tiled ||| Mirror tiled ||| Full
-  where
-    -- default tiling algorithm partitions the screen into two panes
-    tiled   = Tall nmaster delta ratio
-    -- The default number of windows in the master pane
-    nmaster = 1
-    -- Default proportion of screen occupied by master pane
-    ratio   = 1/2
-    -- Percent of screen to increment by when resizing panes
-    delta   = 3/100
+defaultLayouts = layoutHook gnomeConfig
  
 -- Define layout for specific workspaces
-webLayout    = reflectHoriz $ withIM (1%3) (ClassName "Birdie") Grid
+webLayout    = reflectHoriz $ withIM (1%3) (ClassName "Birdie") Grid 
 imLayout     = withIM (1%4) (Role "contact_list") Grid
-systemLayout = Grid ||| Full 
+systemLayout = Grid ||| Full
  
 -- Put all layouts together
 myLayoutHook  = onWorkspace "2:web"     webLayout $
