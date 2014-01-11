5b8b3e06dc388c7f9a8cd169e03be5772b3418db
Lincoln Mullen <lincoln@lincolnmullen.com>
2014-01-11 15:06:56 -0500

    Moved zsh aliases into their own file

---
+++ zsh/aliases.zsh
@@ -0,0 +1,50 @@
+# Projects
+# -------------------------------------------------------------------
+alias blog="cd ~/dev/lincolnmullen.com && gvim -S 2>/dev/null"
+alias diss="cd ~/acad/dissertation && gvim -S 2>/dev/null"
+alias dot="cd ~/dev/dotfiles && gvim -S 2>/dev/null"
+alias jsr="cd ~/dev/jsr && gvim -S 2>/dev/null"
+alias research="cd ~/acad/research/wiki/wikidata && gvim -S 2>/dev/null"
+alias todo="cd ~/todo && gvim -S 2>/dev/null"
+alias notes="cd ~/notes && gvim -S 2>/dev/null"
+
+# File and system management
+# -------------------------------------------------------------------
+alias R="R --no-restore-data --no-save --quiet"
+alias ack="ack-grep"
+alias alert='notify-send -i /usr/share/icons/gnome/32x32/apps/gnome-terminal.png "[$?] Your job is finished"'
+alias duh='du -hs * | sort -h -r'  # Sort files/dirs by size
+alias findd="find . -type d -iname" # find a directory
+alias findf="find . -type f -iname" # find a file 
+alias gvim="gvim 2>/dev/null"
+alias ip="curl icanhazip.com"       # get current public IP
+alias ll="ls -l -F --color --group-directories-first"
+alias ls="ls -F --color --group-directories-first"
+alias more='more -R'                # give more colors
+alias open="xdg-open"
+alias process="ps aux | grep"
+alias runwiki='cd ~/acad/research/wiki && gitit -f my.conf > /dev/null 2>&1 &'
+alias scanlocal='nmap -sP 192.168.1.0/24'
+alias timelog='tail ~/todo/time-use.txt'
+alias tn="new-todo.rb"
+alias tu="timeuse.rb"
+alias zshreload='source ~/.zshrc'
+
+# Git 
+# -------------------------------------------------------------------
+alias ga='git add'
+alias gb='git branch'
+alias gc='git commit'
+alias gca='git commit -a'
+alias gcam='git commit -a -m'
+alias gcm='git commit -m'
+alias gco='git checkout'
+alias gd='git diff'
+alias gi="echo $1 >> .gitignore"
+alias gl='git pull --prune'
+alias glast='git diff HEAD^ HEAD' # diff of last commit
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gp='git push origin HEAD'
+alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+alias gs='git status -sb'
+
