140c192494b0baf14986c018c4ea006a00b50cba
Lincoln Mullen <lincoln@lincolnmullen.com>
2019-06-04 21:21:33 -0400

    Git pull fast forward

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -23,6 +23,7 @@ alias gcam='git commit -a -m'
 alias gcm='git commit -m'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gs='git status -sb'
+alias gl='git pull --ff-only'
 
 # Environment variables
 # -------------------------------------------------------------------
