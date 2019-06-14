7a5574e395cfbae9d3bde8a87882aace86276d64
Lincoln Mullen <lincoln@lincolnmullen.com>
2019-06-14 15:35:48 -0400

    Remove old scripts

--- bin/install-r-package.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-pkg=`echo $1 | tr '[:upper:]' '[:lower:]'`
-if apt-get install r-cran-$pkg; then
-  echo "Successfully installed $pkg from c2d4u"
-else
-  Rscript -e "install.packages('$1', repo = 'https://cran.rstudio.com')"
-fi
-
