27445d9b6e41b3c28cecb18465021aca00934568
Lincoln Mullen <lincoln@lincolnmullen.com>
2019-06-17 11:48:42 -0400

    Update backup scripts

--- bin/backup-home.sh
+++ bin/backup-home.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
-# Backup RAID array to external 4TB disk
-echo "Backing up home at /home/lmullen to external hard disk"
-mkdir -p /media/lmullen/BACKUP-RAID/HOME
-rsync -avP --delete /home/lmullen/* /media/lmullen/BACKUP-RAID/HOME/
+# Backup home directory to external 4TB disk
+DATADIR=/home/lmullen
+BACKUPDIR=/media/backup/HOME
+
+echo "Backing up home directory at $DATADIR to $BACKUPDIR."
+echo "Note that this does not include docker-store and other files."
+
+mkdir -p $BACKUPDIR
+rsync -avP --progress --stats --delete --delete-excluded \
+  --exclude go --exclude R --exclude snap --exclude Templates --exclude texmf \
+  --exclude docker-store \
+  $DATADIR/* $BACKUPDIR
 
--- bin/backup-raid.sh
+++ bin/backup-raid.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
 # Backup RAID array to external 4TB disk
-echo "Backing up RAID at /media/data to external hard disk."
-echo "NB: This does not include the Postgres database."
+DATADIR=/data
 BACKUPDIR=/media/backup/DATA
+
+echo "Backing up RAID at $DATADIR to $BACKUPDIR."
+echo "NB: This does not include the Postgres database which must be backed up separately."
+
 mkdir -p $BACKUPDIR
-rsync -avp --progress --stats --delete --exclude=postgresql --exclude=chronam-wget /media/data/* $BACKUPDIR
+rsync -avp --progress --stats --delete --exclude=postgresql --exclude=chronam-wget $DATADIR/* $BACKUPDIR 
 
