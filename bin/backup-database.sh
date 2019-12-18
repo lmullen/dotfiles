6dbafcfbecf43fc088ea96067dd3202b975b2b00
Lincoln Mullen <lincoln@lincolnmullen.com>
2019-12-18 13:42:13 -0500

    Fix DB backup script

--- bin/backup-database.sh
+++ bin/backup-database.sh
@@ -2,11 +2,16 @@
 
 set +x
 
-# Backup postgres database to external 4TB disk
-echo "Backing up Postgres database to external hard disk"
-mkdir -p /media/lmullen/BACKUP-RAID/postgres
-pg_dump --format=c --compress=9 -v \
-  --file=/media/lmullen/BACKUP-RAID/postgres/researchdb-backup-$(date -Is).dump \
-  lmullen
+# Backup PostgreSQL database
+echo "Backing up PostgreSQL database"
+
+# Directory is named by date
+OUTDIR=/data/backups-db/$(date -Id)
+mkdir -p $OUTDIR
+
+# Clean up the directory in case we already tried backing up today
+# rm -r $OUTDIR/*
+
+pg_dump --format=d --jobs=6 --compress=5 --verbose --file=$OUTDIR lmullen
 
 
