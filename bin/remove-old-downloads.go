6fab9e15fbc14a4ae159316c960237fbfa6c7cf8
Lincoln Mullen <lincoln@lincolnmullen.com>
2025-01-23 11:25:11 -0500

    Improve dowloads cleaning up script

--- bin/remove-old-downloads.go
+++ bin/remove-old-downloads.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log/slog"
 	"os"
 	"path/filepath"
 	"time"
@@ -21,94 +22,84 @@ func formatSize(bytes int64) string {
 	return fmt.Sprintf("%.1f %cB", float64(bytes)/float64(div), "KMGTPE"[exp])
 }
 
-func formatDuration(d time.Duration) string {
-	days := int(d.Hours() / 24)
-	hours := int(d.Hours()) % 24
-	if days > 0 {
-		if hours > 0 {
-			return fmt.Sprintf("%dd %dh old", days, hours)
-		}
-		return fmt.Sprintf("%dd old", days)
-	}
-	return fmt.Sprintf("%dh old", hours)
-}
-
 func main() {
-	dryRun := flag.Bool("dry-run", false, "Show what would be moved without actually moving files")
+	dryRun := flag.Bool("dry-run", false, "Show what would be moved to the trash without actually moving files")
 	flag.Parse()
+
+	opts := &slog.HandlerOptions{
+		Level:     slog.LevelInfo,
+		AddSource: false,
+	}
+	logger := slog.New(slog.NewTextHandler(os.Stdout, opts))
+	slog.SetDefault(logger)
 
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
-		fmt.Printf("Error getting home directory: %v\n", err)
+		logger.Error("failed to get home directory", "error", err)
 		return
 	}
 
 	downloadsDir := filepath.Join(homeDir, "Downloads")
-
-	if *dryRun {
-		fmt.Println("DRY RUN MODE - No files will be moved")
-		fmt.Println("----------------------------------------")
-	}
-
 	now := time.Now()
+	ageThreshold := 24 * time.Hour
+
+	logger.Info(fmt.Sprintf("checking ~/Downloads folder for files and moving files older than %v to trash", ageThreshold))
+
 	var filesFound int
 	var totalSize int64
 
 	err = filepath.Walk(downloadsDir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
-			return err
+			logger.Error("failed to access path", "path", path, "error", err)
+			return nil
 		}
 
 		if info.IsDir() {
 			if path != downloadsDir {
-				fmt.Printf("Skipping directory: %s\n", info.Name())
+				logger.Info("skipping directory", "name", info.Name())
 			}
 			return nil
 		}
 
 		age := now.Sub(info.ModTime())
-		if age > 24*time.Hour {
+		attrs := []any{
+			"name", info.Name(),
+			"size", formatSize(info.Size()),
+			"age", age.String(),
+		}
+
+		if age > ageThreshold {
 			filesFound++
 			totalSize += info.Size()
 
 			if *dryRun {
-				fmt.Printf("Would move: %s (size: %s, age: %s)\n",
-					info.Name(),
-					formatSize(info.Size()),
-					formatDuration(age))
+				logger.Info("would move file", attrs...)
 			} else {
 				trashPath := filepath.Join(homeDir, ".Trash", info.Name())
-				err := os.Rename(path, trashPath)
-				if err != nil {
-					fmt.Printf("Error moving %s to trash: %v\n", info.Name(), err)
+				if err := os.Rename(path, trashPath); err != nil {
+					logger.Error("failed to move file to trash", append(attrs, "error", err)...)
 					return nil
 				}
-				fmt.Printf("Moved: %s (size: %s, age: %s)\n",
-					info.Name(),
-					formatSize(info.Size()),
-					formatDuration(age))
+				logger.Info("moved file to trash", attrs...)
 			}
 		} else {
-			fmt.Printf("Skipping: %s (size: %s, age: %s)\n",
-				info.Name(),
-				formatSize(info.Size()),
-				formatDuration(age))
+			logger.Info("skipping file", attrs...)
 		}
-
 		return nil
 	})
 
 	if err != nil {
-		fmt.Printf("Error walking through Downloads directory: %v\n", err)
+		logger.Error("failed to walk directory", "error", err)
 		return
 	}
 
-	fmt.Println("----------------------------------------")
+	summary := []any{
+		"count", filesFound,
+		"total_size", formatSize(totalSize),
+	}
 	if *dryRun {
-		fmt.Printf("Found %d files that would be moved to trash (total size: %s)\n",
-			filesFound, formatSize(totalSize))
+		logger.Info("dry run complete", summary...)
 	} else {
-		fmt.Printf("Moved %d files to trash (total size: %s)\n",
-			filesFound, formatSize(totalSize))
+		logger.Info("cleanup complete", summary...)
 	}
 }
